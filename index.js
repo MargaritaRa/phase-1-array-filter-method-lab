37e3ba1b95fbad5212712e37e87018360bc09ebf
Margarita Ramirez <margarita26.mr@gmail.com>
2024-03-18 17:15:52 -0400

    pass

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+function findMatching(drivers, name){
+    const lowerCaseName = name.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase() === lowerCaseName);
+
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
+
+
+
